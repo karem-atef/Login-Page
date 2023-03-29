4abc7c17a40e5896788ccb469aab35daf58e0d3a
karamAtef <71832115+karem941@users.noreply.github.com>
2023-03-29 13:46:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7
+.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Login Form</title>
+</head>
+
+<body>
+    <div class="container">
+       
+        <form class="login-email">
+            <p class="login-text"  style="font-size: 2rm; font-weidth:800">Login </p>
+            <div class="input-group">
+                <input type="email" placeholder="Email" required>
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <div class="input-group">
+                <button class="btn">Login</button>
+            </div>
+            <p class="login-register-text">Do not have an account?<a href="register.php"> Register Here.</a></p>
+        </form>
+    </div>
+</body>
+
+</html>
